fe15286264cd65b94a532d8e78cd9781e6e90e16
marloquemegusta <malfonsoars@gmail.com>
2020-09-09 10:17:27 +0200

    train_test_split.py modified so it now takes the img fle extension as a parameter so it can use jpg, png, etc .

--- scripts/train_test_split.py
+++ scripts/train_test_split.py
@@ -18,8 +18,11 @@ ap.add_argument("-vr", "--validation_rate", required=True, type=float,
 ap.add_argument("-tr", "--test_rate", required=True, type=float,
                 help="fraction of the data to be used as test")
 
+ap.add_argument("-f", "--image_format", required=True, type=str,
+                help="format o the image (jpg,png,JPG...")
 
-def partition_dataset(input_dir, output_dir, test_rate, val_rate):
+
+def partition_dataset(input_dir, output_dir, test_rate, val_rate,image_format="jpg"):
     if not glob.glob(input_dir):
         sys.exit("input dir does not exist")
     if not glob.glob(output_dir):
@@ -42,22 +45,22 @@ def partition_dataset(input_dir, output_dir, test_rate, val_rate):
     for val_name in val_names:
         copyfile(input_dir + "/" + val_name + ".json",
                  output_dir + "/val/" + val_name + ".json")
-        copyfile(input_dir + "/" + val_name + ".jpg",
-                 output_dir + "/val/" + val_name + ".jpg")
+        copyfile(input_dir + "/" + val_name + "."+image_format,
+                 output_dir + "/val/" + val_name + "."+image_format)
     print("succesfully created validation split using: ", val_rate*100,"% of data")
     for test_name in test_names:
         copyfile(input_dir + "/" + test_name + ".json",
                  output_dir + "/test/" + test_name + ".json")
-        copyfile(input_dir + "/" + test_name + ".jpg",
-                 output_dir + "/test/" + test_name + ".jpg")
+        copyfile(input_dir + "/" + test_name + "."+image_format,
+                 output_dir + "/test/" + test_name + "."+image_format)
     print("succesfully created test split using: ", test_rate*100,"% of data")
     for train_name in train_names:
         copyfile(input_dir + "/" + train_name + ".json",
                  output_dir + "/train/" + train_name + ".json")
-        copyfile(input_dir + "/" + train_name + ".jpg",
-                 output_dir + "/train/" + train_name + ".jpg")
+        copyfile(input_dir + "/" + train_name + "."+image_format,
+                 output_dir + "/train/" + train_name + "."+image_format)
     print("succesfully created train split using: ", (1-test_rate-val_rate)*100,"% of data")
 
 if __name__ == "__main__":
     args = vars(ap.parse_args())
-    partition_dataset(args["input_dir"], args["output_dir"], args["test_rate"],args["validation_rate"])
+    partition_dataset(args["input_dir"], args["output_dir"], args["test_rate"],args["validation_rate"],args["image_format"])
